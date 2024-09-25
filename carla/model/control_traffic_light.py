923263966b413b89493e59b174b82bb13b2ea002
RamRaid <71435969+ramraid319@users.noreply.github.com>
2024-09-25 23:12:45 +0900

    Update control_traffic_light.py

--- carla/model/control_traffic_light.py
+++ carla/model/control_traffic_light.py
@@ -15,16 +15,6 @@ import carla
 
 from carla import VehicleLightState as vls
 
-import argparse
-import logging
-from numpy import random
-
-traffic_light_01_name = "BP_TrafficLightNew_T10_master_largeBIG_rsc"
-traffic_light_02_name = "BP_TrafficLightNew_T10_master_largeBIG_rsc2"
-traffic_light_03_name = "BP_TrafficLightNew_T10_master_largeBIG_rsc3"
-traffic_light_04_name = "BP_TrafficLightNew_T10_master_largeBIG_rsc4"
-
-
 class carlaEnv:
 
     def __init__(self):
@@ -78,7 +68,18 @@ class carlaEnv:
             traffic_light_actor.set_red_time(list_red_time[idx])
             idx = idx + 1
             print(f"\t\t>> '{traffic_light_actor}' : set red time '{traffic_light_actor.get_red_time()}' --> '{list_red_time[idx]}'")
+    
+    def setTrafficLightListGreen(self, list_target_traffic_light_actor):
+        for traffic_light_actor in list_target_traffic_light_actor:
+            traffic_light_actor.set_state(carla.TrafficLightState.Green)
+            print(f"\t\t>> '{traffic_light_actor}' : set green")
             
+    
+    def setTrafficLightListRed(self, list_target_traffic_light_actor):
+        for traffic_light_actor in list_target_traffic_light_actor:
+            traffic_light_actor.set_state(carla.TrafficLightState.Red)
+            print(f"\t\t>> '{traffic_light_actor}' : set red")
+    
     def allGreen(self):
         for traffic_light_actor in self.list_traffic_light_actor:
             if isinstance(traffic_light_actor, carla.TrafficLight):
@@ -94,22 +95,7 @@ class carlaEnv:
                 actor_.set_red_time(1000.0)
         print(f"\t\t>> now all green..")
 
-    def set_traffic_light_state_by_name(self, name, state):
-        # 모든 액터 가져오기
-        actors = self.world.get_actors()
-        
-        # 이름으로 신호등 찾기
-        for actor in actors:
-            if isinstance(actor, carla.TrafficLight) and actor.get_display_name() == name:
-                actor.set_state(state)
-                actor.set_manual_control(True)  # 수동 제어 모드로 설정
-                print(f"신호등 '{name}' 상태를 {state}로 설정했습니다.")
-                return
-        
-        print(f"이름 '{name}'의 신호등을 찾을 수 없습니다.")
-
 def main():
-    # 예시: 추가적으로 필요한 동작을 main에 작성할 수 있습니다.
     print("running control_traffic_light..")
     
     Env = carlaEnv()

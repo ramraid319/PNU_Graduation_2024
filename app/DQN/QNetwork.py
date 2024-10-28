3183aa5a6f91a7f61a700e16563509dc6379dcb1
bluespaine <104207933+bluespaine@users.noreply.github.com>
2024-10-28 14:20:17 +0900

    Add files via upload
    10.28. 최신화

--- app/DQN/QNetwork.py
+++ app/DQN/QNetwork.py
@@ -72,3 +72,48 @@ class QNet(nn.Module):
         x = self.fc3(x)
         
         return x
+    
+
+# class QNet(nn.Module):
+#     def __init__(self, action_size):
+#         super().__init__()
+
+#         # Convolutional layers to process 200x600x1 input
+#         self.conv1 = nn.Conv2d(1, 32, kernel_size=5, stride=2, padding=2)
+#         self.bn1 = nn.BatchNorm2d(32)
+
+#         self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=2, padding=1)
+#         self.bn2 = nn.BatchNorm2d(64)
+
+#         self.conv3 = nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1)
+#         self.bn3 = nn.BatchNorm2d(128)
+
+#         self.conv4 = nn.Conv2d(128, 256, kernel_size=3, stride=2, padding=1)
+#         self.bn4 = nn.BatchNorm2d(256)
+
+#         self.conv5 = nn.Conv2d(256, 512, kernel_size=3, stride=2, padding=1)
+#         self.bn5 = nn.BatchNorm2d(512)
+
+#         self.fc1 = nn.Linear(512 * 12 * 16, 2048)  # First fully connected layer
+#         self.fc2 = nn.Linear(2048, action_size)  # Output layer for Q-values (4 actions)
+
+#     def forward(self, x):
+#         # Apply convolutional layers with ReLU activations
+#         x = F.relu(self.bn1(self.conv1(x)))
+#         x = F.relu(self.bn2(self.conv2(x)))
+#         x = F.relu(self.bn3(self.conv3(x)))
+#         x = F.relu(self.bn4(self.conv4(x)))
+#         x = F.relu(self.bn5(self.conv5(x)))
+
+#         # Flatten the output from convolutional layers
+#         x = x.view(x.size(0), -1)
+
+#         # Fully connected layers with ReLU activations
+#         x = F.relu(self.fc1(x))
+
+#         # Output Q-values for each action
+#         x = self.fc2(x)
+        
+#         return x
+
+    
